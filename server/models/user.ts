16da9117538b98ae60ed247024a1c24a59d6f388
Fearium <jessebaril.programmer@gmail.com>
2016-03-17 14:02:13 -0400

    updated index.ejs

---
+++ server/models/user.ts
@@ -0,0 +1,43 @@
+import mongoose = require('mongoose');
+
+
+// DEFINE THE OBJECT SCHEMA
+var userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'username is required'
+    },
+    password: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'password is required'
+    },
+    email: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'email is required'
+    },
+    displayName: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Display Name is required'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date,
+        default: Date.now
+    }
+},
+    { collection: 'userInfo' });
+
+
+// MAKE THIS PUBLIC SO THE CONTROLLER CAN SEE IT
+export var User = mongoose.model('User', userSchema);
